5ec9406d8766f9e78a47512e5d9bb2a7f03da97f
Gab <minelligabriele1@gmail.com>
2024-11-16 01:14:20 +0100

    Update a_th_cpd2.m
    Ora chiama xfoil solo una volta e fa tutto nella stessa finestra

--- a_th_cpd2.m
+++ a_th_cpd2.m
@@ -1,14 +1,11 @@
 clear;
 clc;
-close all;
-
-addpath functions
+close all
 
 NACA = '6409';
 alpha_vett = 3:0.1:4;
 n = length(alpha_vett);
-numNodes = 160;
-Cp_fileName = 'Cp.dat'; 
+numNodes = 160; 
 
 cp_struct = cell(n, 2);
 d2_struct = cell(n, 1);
@@ -17,30 +14,37 @@ n_points_d2 = 10; % numero  di punti da interpolare per calcolare la derivata se
 
 %%
 
+% write input file
+fid = fopen('xfoil_input.txt','w');
+fprintf(fid,['NACA ' NACA '\n']);
+fprintf(fid,'PPAR\n');
+fprintf(fid,['N ' num2str(numNodes) '\n']);
+fprintf(fid,'\n\n');
+fprintf(fid,'OPER\n');
+
 for i = 1:n
     alpha = alpha_vett(i);
-
+    Cp_fileName = ['Cp' num2str(i) '.dat'];
     if (exist(Cp_fileName,'file'))
         delete(Cp_fileName);
     end
-    
-    fid = fopen('xfoil_input.txt','w');
-    fprintf(fid,['NACA ' NACA '\n']);
-    fprintf(fid,'PPAR\n');
-    fprintf(fid,['N ' num2str(numNodes) '\n']);
-    fprintf(fid,'\n\n');
-    
-    fprintf(fid,'OPER\n');
     fprintf(fid,['Alfa ' num2str(alpha) '\n']);
-    fprintf(fid,['CPWR ' Cp_fileName]);
-    
-    fclose(fid);
-    
-    xfoil_path = fullfile('XFOIL\xfoil.exe');
-    cmd = [xfoil_path, ' < xfoil_input.txt > output.txt'];
-    system(cmd);
-    
+    fprintf(fid,['CPWR ' Cp_fileName '\n']);
+end
+
+fclose(fid);
+
+% run xfoil
+xfoil_path = fullfile('XFOIL\xfoil.exe');
+cmd = [xfoil_path, ' < xfoil_input.txt > output.txt'];
+system(cmd);
+
+% collect data from files and compute second derivative
+for i = 1:n
+    alpha = alpha_vett(i);
+    Cp_fileName = ['Cp' num2str(i) '.dat'];
     cp_table = import_Cp_plot(Cp_fileName);
+    delete(Cp_fileName);
 
     i_separ = 1;
     for j = 2:numNodes
@@ -56,15 +60,14 @@ for i = 1:n
     cp_top = cp_table.cp(1:i_separ-1);
     cp_top = flipud(cp_top);
     cp_bot = cp_table.cp(i_separ:end);
-
+    
     cp_struct{i,1} = [x_top, cp_top];
     cp_struct{i,2} = [x_bot, cp_bot];
-
+    
     [~, d2dx] = der2(x_top, -cp_top, n_points_d2);
     d2_struct{i} = [x_top, d2dx'];
-
-end
-
+end    
+    
 if (exist('xfoil_input.txt','file'))
     delete('xfoil_input.txt');
 end
@@ -73,7 +76,7 @@ if (exist(Cp_fileName,'file'))
     delete(Cp_fileName);
 end
 
-%%
+%% plot
 
 figure(1)
 legend_entries = cell(n,1);
@@ -85,4 +88,4 @@ end
 plot([0 1], [0 0], 'k')
 legend(legend_entries)
 ylim([-20 20])
-
+title(['NACA' NACA]);
